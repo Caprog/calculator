15d71dae6a67ffc09f7ab1a84d9d25fa81169ae5
Bak <bbakam.pro@gmail.com>
2021-07-01 16:17:32 +0200

    petites corrections du code

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,26 +1,27 @@
 public class Calculator {
 
     public float sum(float a, float b) {
-        return 0;
+        return 1;
     }
 
     public float subtract(float a, float b) {
-        return 0;
+        return 1;
     }
 
     public float division(float a, float b) {
-        return 0;
+        return 1;
     }
 
     public float multiply(float a, float b) {
-        return 0;
+        return 1;
     }
 
     public static void main(String[] args) {
+
         Calculator calculator = new Calculator();
-        System.out.println(calculator.sum(1, 1));
-        System.out.println(calculator.subtract(1, 1));
+        System.out.println(calculator.sum(1, 0));
+        System.out.println(calculator.subtract(2, 1));
         System.out.println(calculator.division(2, 2));
-        System.out.println(calculator.multiply(1, 2));
+        System.out.println(calculator.multiply(1, 1));
     }
 }
