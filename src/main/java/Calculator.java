b0998eb68578ccd8ca0a5d1ffeedb55784c22cec
Bak <bbakam.pro@gmail.com>
2021-07-01 17:04:49 +0200

    les méthodes ( sum/ subt/ multiply/ division ) complétées

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,27 +1,31 @@
 public class Calculator {
 
     public float sum(float a, float b) {
-        return 1;
+        float sum = a + b;
+        return sum;
     }
 
     public float subtract(float a, float b) {
-        return 1;
+        float subtract = a - b;
+        return subtract;
     }
 
     public float division(float a, float b) {
-        return 1;
+        float division = a / b;
+        return division;
     }
 
     public float multiply(float a, float b) {
-        return 1;
+        float multiply = a * b;
+        return multiply;
     }
 
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
-        System.out.println(calculator.sum(1, 0));
-        System.out.println(calculator.subtract(2, 1));
+        System.out.println(calculator.sum(1, 1));
+        System.out.println(calculator.subtract(1, 1));
         System.out.println(calculator.division(2, 2));
-        System.out.println(calculator.multiply(1, 1));
+        System.out.println(calculator.multiply(1, 2));
     }
 }
